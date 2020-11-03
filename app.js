a719b0696793eecdad7c304f86aebf28fc632fba
Your Name <darwinmuthu450@gmail.com>
2020-11-03 18:01:09 +0530

    routes refactored

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -17,8 +17,22 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
-});
-app.post('/api/v1/tours', (req, res) => {
+};
+
+const getTour = (req, res) => {
+  const id = Number(req.params.id);
+  let tour;
+  if ((tour = tours.find((e) => e.id === id))) {
+    res.status(200).json({
+      status: 'success',
+      tour: tour,
+    });
+  } else {
+    res.status(404).send('not found');
+  }
+};
+
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign(
     {
@@ -26,7 +40,6 @@ app.post('/api/v1/tours', (req, res) => {
     },
     req.body
   );
-
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
@@ -38,9 +51,9 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   const id = Number(req.params.id);
   let tour;
   if ((tour = tours.find((e) => e.id === id))) {
@@ -51,9 +64,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   } else {
     res.status(404).send('not found');
   }
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   const id = Number(req.params.id);
   let tour;
   if ((tour = tours.find((e) => e.id === id))) {
@@ -64,19 +77,24 @@ app.delete('/api/v1/tours/:id', (req, res) => {
   } else {
     res.status(404).send('not found');
   }
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  const id = Number(req.params.id);
-  let tour;
-  if ((tour = tours.find((e) => e.id === id))) {
-    res.status(200).json({
-      status: 'success',
-      tour: tour,
-    });
-  } else {
-    res.status(404).send('not found');
-  }
-});
+// app.get('/api/v1/tours', getAllTours);
+//
+// app.get('/api/v1/tours/:id', getTour);
+//
+// app.post('/api/v1/tours', createTour);
+//
+// app.patch('/api/v1/tours/:id', updateTour);
+//
+// app.delete('/api/v1/tours/:id', deleteTour);
+//
+// app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 app.listen(port, () => console.log(`app listening on port 3000!`));
